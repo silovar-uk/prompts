d26acfbf896e60b1652654e7184633fefc540775
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-14 09:42:54 +0900

    Render enhanced launcher shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
-import App from "./App";
+import EnhancedApp from "./EnhancedApp";
 import "./index.css";
 
 registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <EnhancedApp />
   </React.StrictMode>
 );
