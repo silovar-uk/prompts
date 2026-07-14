13d741e36a23b00e801a429aa6111536443afa3f
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-14 11:27:40 +0900

    Boot the unified launcher and auto-refresh stale caches

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
-import EnhancedApp from "./EnhancedApp";
+import ProductApp from "./ProductApp";
 import "./index.css";
 
-registerSW({ immediate: true });
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    void updateSW(true);
+  },
+  onRegisteredSW(_swUrl, registration) {
+    void registration?.update();
+    window.setInterval(() => void registration?.update(), 60 * 60 * 1000);
+  }
+});
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <EnhancedApp />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<ProductApp />);
