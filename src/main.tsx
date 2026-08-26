6832eecc0190f0f90a38fccfee9e1c6d7bd7b107
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-08-26 12:17:32 +0900

    Split reference and launcher bundles by mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
-import LibraryFirstApp from "./LibraryFirstApp";
-import ReferenceLibraryApp from "./ReferenceLibraryApp";
-import "./index.css";
-import "./library-first.css";
-import "./image-prompts.css";
-import "./reference-library.css";
-import "./reference-cards.css";
 
 const legacyMode = new URLSearchParams(window.location.search).get("mode") === "launcher";
 
@@ -22,6 +15,26 @@ triggerServiceWorkerUpdate = registerSW({
   }
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  legacyMode ? <LibraryFirstApp /> : <ReferenceLibraryApp />
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+async function renderApp() {
+  if (legacyMode) {
+    const [{ default: LibraryFirstApp }] = await Promise.all([
+      import("./LibraryFirstApp"),
+      import("./index.css"),
+      import("./library-first.css"),
+      import("./image-prompts.css")
+    ]);
+    root.render(<LibraryFirstApp />);
+    return;
+  }
+
+  const [{ default: ReferenceLibraryApp }] = await Promise.all([
+    import("./ReferenceLibraryApp"),
+    import("./reference-library.css"),
+    import("./reference-cards.css")
+  ]);
+  root.render(<ReferenceLibraryApp />);
+}
+
+void renderApp();
